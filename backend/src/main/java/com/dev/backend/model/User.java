9e9880baef124dae218efacf8dab4ac976aadfac
DevShubham07 <shubham720648@gmail.com>
2025-07-09 00:22:51 +0530

    DB Connectivity

---
+++ backend/src/main/java/com/dev/backend/model/User.java
@@ -0,0 +1,27 @@
+package com.dev.backend.model;
+
+import org.hsqldb.lib.RCData;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // ADMIN, MANAGER, EMPLOYEE
+}
